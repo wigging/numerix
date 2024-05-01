8fa7c10c4ec6cc11a42e5295d111ae4dc590f918
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-04-30 23:11:29 -0400

    Use string format for matrix and vector description

--- Sources/Numerix/Description.swift
+++ Sources/Numerix/Description.swift
@@ -5,16 +5,17 @@
 //  Created by Gavin Wiggins on 3/31/24.
 //
 
+/// String representation of a scalar value such as a Float or Double.
 public protocol ScalarStringConvertible {
     var scalarDescription: String { get }
 }
 
 extension Float: ScalarStringConvertible {
-    public var scalarDescription: String { self.formatted(.number.precision(.fractionLength(1))) }
+    public var scalarDescription: String { String(format: "%6.2f", self) }
 }
 
 extension Double: ScalarStringConvertible {
-    public var scalarDescription: String { self.formatted(.number.precision(.fractionLength(2))) }
+    public var scalarDescription: String { String(format: "%8.4f", self) }
 }
 
 extension Vector: CustomStringConvertible where T: ScalarStringConvertible {
@@ -22,7 +23,7 @@ extension Vector: CustomStringConvertible where T: ScalarStringConvertible {
     public var description: String {
         let desc = """
         \(values.count)-element \(type(of: self))
-         \(values.map(\.scalarDescription).joined(separator: "  "))
+        \(values.map(\.scalarDescription).joined(separator: " "))
         """
         return desc
     }
@@ -34,9 +35,9 @@ extension Matrix: CustomStringConvertible where T: ScalarStringConvertible {
         var vals = ""
         for i in 0..<rows {
             let rowValues = (0..<columns).map { self[i, $0] }
-            let rowString = rowValues.map(\.scalarDescription).joined(separator: "  ")
+            let rowString = rowValues.map(\.scalarDescription).joined(separator: " ")
             let end = i < rows - 1 ? "\n" : ""
-            vals += " " + rowString + end
+            vals += rowString + end
         }
         let desc = """
         \(rows)x\(columns) \(type(of: self))
--- Tests/DescriptionTests.swift
+++ Tests/DescriptionTests.swift
@@ -14,18 +14,35 @@ final class DescriptionTests: XCTestCase {
         let mat = Matrix(rows: 2, columns: 3, values: [3, 4, 8, 10, 11, 18.2])
         let desc = """
         2x3 Matrix<Double>
-         3.00  4.00  8.00
-         10.00  11.00  18.20
+          3.0000   4.0000   8.0000
+         10.0000  11.0000  18.2000
         """
         XCTAssertEqual(String(describing: mat), desc)
+
+        let mat2 = Matrix<Float>(rows: 2, columns: 3, values: [3, 4, 8, 10, 11, 18.2])
+        let desc2 = """
+        2x3 Matrix<Float>
+          3.00   4.00   8.00
+         10.00  11.00  18.20
+        """
+        XCTAssertEqual(String(describing: mat2), desc2)
     }
 
     func testVectorDescription() {
         let vec = Vector([3, 5.8, 402.89, 1])
+        print(vec)
         let desc = """
         4-element Vector<Double>
-         3.00  5.80  402.89  1.00
+          3.0000   5.8000 402.8900   1.0000
         """
         XCTAssertEqual(String(describing: vec), desc)
+
+        let vec2 = Vector<Float>([3, 5.8, 402.89, 1])
+        print(vec2)
+        let desc2 = """
+        4-element Vector<Float>
+          3.00   5.80 402.89   1.00
+        """
+        XCTAssertEqual(String(describing: vec2), desc2)
     }
 }
