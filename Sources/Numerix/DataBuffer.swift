953c78842254e81569453e3d6c5d937280cbff72
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-07-17 23:07:57 -0400

    Create mutable memory buffer with allocate capacity method

--- Sources/Numerix/DataBuffer.swift
+++ Sources/Numerix/DataBuffer.swift
@@ -7,27 +7,21 @@ class DataBuffer<T> {
     var buffer: UnsafeMutableBufferPointer<T>
 
     init(count: Int, fill: T) {
-        let start = UnsafeMutablePointer<T>.allocate(capacity: count)
-        self.buffer = UnsafeMutableBufferPointer(start: start, count: count)
+        self.buffer = UnsafeMutableBufferPointer<T>.allocate(capacity: count)
         self.buffer.initialize(repeating: fill)
     }
 
     init(count: Int) {
-        let start = UnsafeMutablePointer<T>.allocate(capacity: count)
-        self.buffer = UnsafeMutableBufferPointer(start: start, count: count)
+        self.buffer = UnsafeMutableBufferPointer<T>.allocate(capacity: count)
     }
 
     init(array: [T]) {
-        let count = array.count
-        let start = UnsafeMutablePointer<T>.allocate(capacity: count)
-        self.buffer = UnsafeMutableBufferPointer(start: start, count: count)
+        self.buffer = UnsafeMutableBufferPointer<T>.allocate(capacity: array.count)
         _ = self.buffer.initialize(fromContentsOf: array)
     }
 
     init(buffer: UnsafeMutableBufferPointer<T>) {
-        let count = buffer.count
-        let start = UnsafeMutablePointer<T>.allocate(capacity: count)
-        self.buffer = UnsafeMutableBufferPointer(start: start, count: count)
+        self.buffer = UnsafeMutableBufferPointer<T>.allocate(capacity: buffer.count)
         _ = self.buffer.initialize(from: buffer)
     }
 
